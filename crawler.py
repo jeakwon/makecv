bb5f84a920c446fb32982717c87154ab182ec96c
jeakwon <33548421+jeakwon@users.noreply.github.com>
2021-12-13 11:13:07 +0900

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -6,7 +6,7 @@ import json
 import os
 import sys
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname('.')
 
 print('뉴스기사 스크래핑 시작')
 
@@ -15,17 +15,17 @@ req.encoding= None
 html = req.content
 soup = BeautifulSoup(html, 'html.parser')
 datas = soup.select(
-    'div.content01 > div > ul > li:nth-child(1) > article' 
+    'div.contents > div.content01 > div > ul > li > article > div > h3'
     )
 
 data = {}
-
 for title in datas:   
     name = title.find_all('a')[0].text
     url = 'http:'+title.find('a')['href']
     data[name] = url
 
 with open(os.path.join(BASE_DIR, 'news.json'), 'w+',encoding='utf-8') as json_file:
+    print(data)
     json.dump(data, json_file, ensure_ascii = False, indent='\t')
 
 print('뉴스기사 스크래핑 끝')
